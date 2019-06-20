82f7e32775b47513966682da4dafd41f71676487
angel prats <angelpratz@gmail.com>
2019-06-20 08:38:45 -0400

    db challenge 3 initial commit

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const schoolRouter = require('../school/school-router.js')
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('./api/schools', schoolRouter);
+
+module.export = server;
